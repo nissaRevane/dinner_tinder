a4930cfaa6a666cfa0e31eb6da1a2924b0a7cd8f
Quentin <girard.quentin359@gmail.com>
2022-04-10 13:01:12 +0200

    [INIT] Setup ember-cli-rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount_ember_app :frontend, to: '/'
+
   resources :ingredients, only: %i[index show]
   resources :recipe_ingredients, only: %i[index show]
   resources :recipes, only: %i[index show]
